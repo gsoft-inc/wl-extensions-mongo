6930e2e44d64391e0911cfbda728dbb5b8d89bc8
Anthony Simmon <anthony.simmon@gsoft.com>
2022-06-07 14:43:48 -0400

    Added tests

---
+++ src/GSoft.Infra.Mongo.Tests/EphemeralMongoBuilderExtensions.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Options;
+using Mongo2Go;
+
+namespace GSoft.Infra.Mongo.Tests;
+
+public static class EphemeralMongoBuilderExtensions
+{
+    /// <summary>
+    /// Provides a real implementation of MongoDB using a ephemeral localhost server that will be destroyed
+    /// when it will no longer be used by any running test. The first startup can take approximately two seconds on a dev workstation.
+    /// https://github.com/Mongo2Go/Mongo2Go
+    /// </summary>
+    public static MongoBuilder UseEphemeralRealServer(this MongoBuilder builder)
+    {
+        builder.Services.AddSingleton<PrivateMongoDbRunner>();
+        builder.Services.ConfigureOptions<EphemeralMongoSetup>();
+
+        return builder;
+    }
+
+    private sealed class PrivateMongoDbRunner : IDisposable
+    {
+        private static readonly object _lockObj = new object();
+        private static MongoDbRunner? _runner;
+        private static int _useCount;
+
+        public PrivateMongoDbRunner()
+        {
+            lock (_lockObj)
+            {
+                // The lock and use count prevent multiple instances of local MongoDB that would degrade the overall performance
+                _runner ??= MongoDbRunner.Start(singleNodeReplSet: true, logger: NullLogger.Instance);
+                _useCount++;
+
+                this.ConnectionString = _runner.ConnectionString;
+            }
+        }
+
+        public string ConnectionString { get; }
+
+        public void Dispose()
+        {
+            lock (_lockObj)
+            {
+                _useCount--;
+                if (_useCount == 0 && _runner != null)
+                {
+                    _runner.Dispose();
+                    _runner = null;
+                }
+            }
+        }
+    }
+
+    private sealed class EphemeralMongoSetup : IConfigureOptions<MongoOptions>
+    {
+        private readonly PrivateMongoDbRunner _runner;
+
+        public EphemeralMongoSetup(PrivateMongoDbRunner runner)
+        {
+            this._runner = runner;
+        }
+
+        public void Configure(MongoOptions options)
+        {
+            // Each test that requests a IMongoDatabase will have its own separate database and storage
+            options.ConnectionString = this._runner.ConnectionString;
+            options.DefaultDatabaseName = Guid.NewGuid().ToString("N");
+        }
+    }
+}
---
+++ src/GSoft.Infra.Mongo.Tests/MongoBasicTests.cs
@@ -0,0 +1,61 @@
+﻿using GSoft.Xunit.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace GSoft.Infra.Mongo.Tests;
+
+public sealed class MongoBasicTests : BaseIntegrationTest<MongoFixture>
+{
+    public MongoBasicTests(MongoFixture fixture, ITestOutputHelper testOutputHelper)
+        : base(fixture, testOutputHelper)
+    {
+    }
+
+    [Fact]
+    public async Task Dates_Are_Always_Serialized_In_Utc()
+    {
+        var dtNow = DateTime.Now;
+        var dtUtcNow = dtNow.ToUniversalTime();
+        var dtoNow = DateTimeOffset.Now;
+        var dtoUtcNow = dtoNow.ToUniversalTime();
+
+        var brandsToInsert = new List<CarBrandDocument>
+        {
+            new CarBrandDocument { Name = "Toyota", DateTimeProperty = dtNow, DateTimeOffsetProperty = dtoNow },
+            new CarBrandDocument { Name = "Honda", DateTimeProperty = dtUtcNow, DateTimeOffsetProperty = dtoUtcNow },
+        };
+
+        var collection = this.Services.GetRequiredService<IMongoDatabase>().GetCollection<CarBrandDocument>();
+        await collection.InsertManyAsync(brandsToInsert);
+
+        var insertedBrands = await collection.Find(FilterDefinition<CarBrandDocument>.Empty).ToAsyncEnumerable().ToListAsync();
+
+        Assert.Equal(brandsToInsert.Count, insertedBrands.Count);
+        var toyota = Assert.Single(insertedBrands, x => x.Name == "Toyota");
+        var honda = Assert.Single(insertedBrands, x => x.Name == "Honda");
+
+        Assert.Equal(dtUtcNow, toyota.DateTimeProperty);
+        Assert.Equal(dtoUtcNow, toyota.DateTimeOffsetProperty);
+
+        Assert.Equal(dtUtcNow, honda.DateTimeProperty);
+        Assert.Equal(dtoUtcNow, honda.DateTimeOffsetProperty);
+    }
+
+    // BsonType.Int64 (ticks) and BsonType.Array (ticks, offset) are used to have the best precision
+    // By default, we store dates as MongoDB datetimes which are precise to the millisecond
+    [MongoCollection("carBrands")]
+    private sealed class CarBrandDocument : MongoDocument
+    {
+        public string Name { get; set; } = string.Empty;
+
+        [BsonRepresentation(BsonType.Int64)]
+        public new DateTime DateTimeProperty { get; set; }
+
+        [BsonRepresentation(BsonType.Array)]
+        public new DateTimeOffset DateTimeOffsetProperty { get; set; }
+    }
+}
