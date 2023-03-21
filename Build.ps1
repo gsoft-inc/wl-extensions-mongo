c2bc65db97547ddd8a385fbf95d77d144d76ef76
Guillaume Caya-Letourneau <guillaume.caya@gsoft.com>
2023-03-21 15:51:58 +0000

    Merged PR 1372: TECH-251 - Base analyzers structure
    Added base project structure in order to add analyzers later.
    Here's the output in nupkg for abstraction
    ![image.png](https://dev.azure.com/gsoft/46c89666-c1f6-4351-a213-a0a678ed8fe7/_apis/git/repositories/6d987864-9555-470f-aaad-4636f2d15fcb/pullRequests/1372/attachments/image.png)

--- Build.ps1
+++ Build.ps1
@@ -22,8 +22,8 @@ Process {
     
         Exec { & dotnet clean -c Release }
         Exec { & dotnet build -c Release }
-        Exec { & dotnet test  -c Release --no-build -r "$outputDir" --no-restore -l "trx" -l "console;verbosity=detailed" }
-        Exec { & dotnet pack  -c Release --no-build -o "$outputDir" }
+        Exec { & dotnet test  -c Release -r "$outputDir" --no-restore -l "trx" -l "console;verbosity=detailed" }
+        Exec { & dotnet pack  -c Release -o "$outputDir" }
 
         if (($null -ne $env:NUGET_SOURCE ) -and ($null -ne $env:NUGET_API_KEY)) {
             Exec { & dotnet nuget push "$nupkgsPath" -s $env:NUGET_SOURCE -k $env:NUGET_API_KEY }
