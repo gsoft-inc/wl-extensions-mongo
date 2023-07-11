ea960c5630e26e8ce400b13dc2d73dfe08b7d550
Guillaume Caya-Letourneau <guillaume.caya.letourneau@gmail.com>
2023-07-11 10:25:26 -0400

    Update Build.ps1 (#14)

--- Build.ps1
+++ Build.ps1
@@ -22,7 +22,7 @@ Process {
     
         Exec { & dotnet clean -c Release }
         Exec { & dotnet build -c Release }
-        Exec { & dotnet test  -c Release -r "$outputDir" --no-restore -l "trx" -l "console;verbosity=detailed" }
+        Exec { & dotnet test  -c Release --results-directory "$outputDir" --no-restore -l "trx" -l "console;verbosity=detailed" }
         Exec { & dotnet pack  -c Release -o "$outputDir" }
 
         if (($null -ne $env:NUGET_SOURCE ) -and ($null -ne $env:NUGET_API_KEY)) {
