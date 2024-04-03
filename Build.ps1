6d93d5c2c1e577a4910d9696e6eab09bec673508
He Qian Wang <heqian.wang@gsoft.com>
2024-04-03 16:42:10 -0400

    Uncomment chagnes

--- Build.ps1
+++ Build.ps1
@@ -29,9 +29,9 @@ Process {
         $gitVersion = Exec { & dotnet dotnet-gitversion /output json /showvariable SemVer }
         $version = "$gitVersion.$uniqueId"
 
-        # Exec { & dotnet clean -c Release }
-        # Exec { & dotnet build -c Release }
-        # Exec { & dotnet test  -c Release --results-directory "$outputDir" --no-restore -l "trx" -l "console;verbosity=detailed" }
+        Exec { & dotnet clean -c Release }
+        Exec { & dotnet build -c Release }
+        Exec { & dotnet test  -c Release --results-directory "$outputDir" --no-restore -l "trx" -l "console;verbosity=detailed" }
         Exec { & dotnet pack  -c Release -o "$outputDir" /p:Version=$version }
 
         if (($null -ne $env:NUGET_SOURCE ) -and ($null -ne $env:NUGET_API_KEY)) {
