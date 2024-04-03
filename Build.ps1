ce5779720cce2a76b0df02b2c7693a2f1cc442fa
He Qian Wang <heqian.wang@gsoft.com>
2024-04-03 18:15:39 -0400

    Use gitversion changes to govern package versions

--- Build.ps1
+++ Build.ps1
@@ -25,18 +25,19 @@ Process {
         Exec { & dotnet tool restore }
 
         # Let GitVersion compute the NuGet package version
-        $shortSHA = Exec { & git rev-parse --short HEAD }
-        $gitVersion = Exec { & dotnet dotnet-gitversion /output json /showvariable SemVer }
-        $version = "$gitVersion.$shortSHA"
+        # $shortSHA = Exec { & git rev-parse --short HEAD }
+        # $gitVersion = Exec { & dotnet dotnet-gitversion /output json /showvariable SemVer }
+        # $version = "$gitVersion.$shortSHA"
 
         Exec { & dotnet clean -c Release }
         Exec { & dotnet build -c Release }
         Exec { & dotnet test  -c Release --results-directory "$outputDir" --no-restore -l "trx" -l "console;verbosity=detailed" }
-        Exec { & dotnet pack  -c Release -o "$outputDir" /p:Version=$version }
+        Exec { & dotnet pack -c Release -o "$outputDir" }
 
         if (($null -ne $env:NUGET_SOURCE ) -and ($null -ne $env:NUGET_API_KEY)) {
             Exec { & dotnet nuget push "$nupkgsPath" -s $env:NUGET_SOURCE -k $env:NUGET_API_KEY }
         }
+        Write-Host "Packing with version: $version"
     }
     finally {
         Pop-Location
