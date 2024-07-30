9b1a725028907a5775a2bbbbabaf2e2ccb8b4fd8
Gérald Barré <gerald.barre@gsoft.com>
2024-07-30 10:25:14 -0400

    Run test sequentially

---
+++ src/Workleap.Extensions.Mongo.Tests/NonParallelCollectionDefinitionClass.cs
@@ -0,0 +1,6 @@
+﻿namespace Workleap.Extensions.Mongo.Tests;
+
+[CollectionDefinition("Non-Parallel Collection", DisableParallelization = true)]
+public sealed class NonParallelCollectionDefinitionClass
+{
+}
