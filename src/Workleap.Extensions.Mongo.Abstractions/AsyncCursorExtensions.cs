dd84dd9fb98e033671fcb9dcc07bf621ab467ca1
Workleap <53535748+Infra-Workleap@users.noreply.github.com>
2024-08-16 14:38:58 -0400

    chore(deps): update dependency dotnet-sdk to v8.0.400 (#102)
    * chore(deps): update dependency dotnet-sdk to v8.0.400
    
    * use global.json instead of explicit .NET 6 version
    
    * Disabled IDE0130
    
    * Keep both .net 6 and 8
    
    * Update src/Workleap.Extensions.Mongo.Abstractions/AsyncCursorExtensions.cs
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: He Qian Wang <heqian.wang@gsoft.com>
    Co-authored-by: heqianwang <158102624+heqianwang@users.noreply.github.com>

--- src/Workleap.Extensions.Mongo.Abstractions/AsyncCursorExtensions.cs
+++ src/Workleap.Extensions.Mongo.Abstractions/AsyncCursorExtensions.cs
@@ -1,7 +1,7 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 
-// ReSharper disable once CheckNamespace
+#pragma warning disable IDE0130 // Namespace does not match folder structure
 namespace MongoDB.Driver;
 
 public static class AsyncCursorExtensions
